1a07ec023c95933d108fcf78f7c0e330215dc711
Tim <voncurleone@gmail.com>
2022-12-29 17:02:22 -0500

    Added a test for the reply with the latest temp reading.

--- src/test/scala/com/iot/DeviceSpec.scala
+++ src/test/scala/com/iot/DeviceSpec.scala
@@ -15,5 +15,32 @@ class DeviceSpec extends ScalaTestWithActorTestKit with AnyWordSpecLike {
       response.requestId should === (0)
       response.value should === (None)
     }
+
+    "reply with latest temp reading" in {
+      //set up device and probe
+      val recordProbe = createTestProbe[TempRecorded]()
+      val readProbe = createTestProbe[RespondTemp]()
+      val device = spawn(Device("grouo:1", "Device:1"))
+
+      //send it a record temp message
+      device ! RecordTemp(1, -1.1, recordProbe.ref)
+      recordProbe.expectMessage(TempRecorded(1))
+
+      //read the value
+      device ! ReadTemp(2, readProbe.ref)
+      val readResponse1 = readProbe.receiveMessage()
+      readResponse1.requestId should === (2)
+      readResponse1.value should === (Some(-1.1))
+
+      //send it a second record temp message
+      device ! RecordTemp(3, 1.1, recordProbe.ref)
+      recordProbe.expectMessage(TempRecorded(3))
+
+      //read the value
+      device ! ReadTemp(4, readProbe.ref)
+      val readResponse2 = readProbe.receiveMessage()
+      readResponse2.requestId should ===(4)
+      readResponse2.value should ===(Some(1.1))
+    }
   }
 }
