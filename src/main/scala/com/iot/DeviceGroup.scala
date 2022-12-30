7b61011d190ab16bc656570660751b4c79409994
Tim <voncurleone@gmail.com>
2022-12-30 16:32:11 -0500

    Added DeviceGroup class.

--- src/main/scala/com/iot/DeviceGroup.scala
+++ src/main/scala/com/iot/DeviceGroup.scala
@@ -1,5 +1,48 @@
 package com.iot
 
+import akka.actor.typed.{ActorRef, Behavior, PostStop, Signal}
+import akka.actor.typed.scaladsl.{AbstractBehavior, ActorContext, Behaviors, LoggerOps}
+
 object DeviceGroup {
   trait Command
+
+  def apply(groupId: String): Behavior[Command] =
+    Behaviors.setup(new DeviceGroup(_, groupId))
+}
+
+class DeviceGroup(context: ActorContext[DeviceGroup.Command], groupId: String)
+  extends AbstractBehavior[DeviceGroup.Command](context) {
+  import DeviceGroup._
+  import DeviceManager.{RequestTrackDevice, DeviceRegistered, RequestDeviceList, ReplyDeviceList}
+
+  private var devices: Map[String, ActorRef[Device.Command]] = Map()
+  context.log.info("DeviceGroup {} started", groupId)
+
+  override def onMessage(msg: DeviceGroup.Command): Behavior[DeviceGroup.Command] = {
+    msg match {
+      case RequestTrackDevice(`groupId`, deviceId, replyTo) =>
+        devices.get(deviceId) match {
+          case Some(deviceActor) =>
+            replyTo ! DeviceRegistered(deviceActor)
+          case None =>
+            //create new device
+            context.log.info("Creating device actor for {}", deviceId)
+            val deviceActor = context.spawn(Device(groupId, deviceId), s"Device: $deviceId")
+            //add new devices to the device map
+            devices += deviceId -> deviceActor
+            replyTo ! DeviceRegistered(deviceActor)
+        }
+        this
+
+      case RequestTrackDevice(gId, _, _) =>
+        context.log.warn2("Ignoring request for {}. This actor is responsible for device group: {}", gId, groupId)
+        this
+    }
+  }
+
+  override def onSignal: PartialFunction[Signal, Behavior[Command]] = {
+    case PostStop =>
+      context.log.info("DeviceGroup {} stopped", groupId)
+      this
+  }
 }
