975e5d06d2bf52e0a60acb68c90f505b83d2148e
Tim <voncurleone@gmail.com>
2022-12-30 17:09:40 -0500

    Added test for registering a device actor in a device group.

--- src/test/scala/com/iot/DeviceSpec.scala
+++ src/test/scala/com/iot/DeviceSpec.scala
@@ -2,8 +2,10 @@ package com.iot
 
 import akka.actor.testkit.typed.scaladsl.ScalaTestWithActorTestKit
 import org.scalatest.wordspec.AnyWordSpecLike
+
 class DeviceSpec extends ScalaTestWithActorTestKit with AnyWordSpecLike {
   import Device._
+  import DeviceManager._
 
   "Device actor" must {
     "reply with empty reading if no temp is known" in {
@@ -42,5 +44,34 @@ class DeviceSpec extends ScalaTestWithActorTestKit with AnyWordSpecLike {
       readResponse2.requestId should ===(4)
       readResponse2.value should ===(Some(1.1))
     }
+  }
+
+  "Device group" must {
+    "be able to register a device actor" in {
+
+      //setup device group and probe
+      val probe = createTestProbe[DeviceRegistered]()
+      val deviceGroup = spawn(DeviceGroup("group:1"))
+
+      //register the probe as the replyTo
+      deviceGroup ! RequestTrackDevice("group:1", "device:1", probe.ref)
+      val registered1 = probe.receiveMessage()
+      val device1 = registered1.device
+
+      //register a second deviceID with the probe
+      deviceGroup ! RequestTrackDevice("group:1", "device:2", probe.ref)
+      val registered2 = probe.receiveMessage()
+      val device2 = registered2.device
+      device1 should !== (device2)
+
+      //check that the device actors are working
+      val recordProbe = createTestProbe[TempRecorded]()
+      device1 ! RecordTemp(0, 5.5, recordProbe.ref)
+      recordProbe.expectMessage(TempRecorded(0))
+      device2 ! RecordTemp(1, -.3, recordProbe.ref)
+      recordProbe.expectMessage(TempRecorded(1))
+    }
+
+    //todo: test for requsting an incorrect group id
   }
 }
