9148bc047cb67fdc35512306dd62716d3173d45f
Tim <voncurleone@gmail.com>
2023-01-03 16:30:09 -0500

    Moved message protocol from DeviceGroupQuery.scala to DeviceManager.scala.

--- src/main/scala/com/iot/DeviceGroupQuery.scala
+++ src/main/scala/com/iot/DeviceGroupQuery.scala
@@ -4,16 +4,4 @@ import akka.actor.typed.ActorRef
 
 object DeviceGroupQuery {
   trait Command
-  protected final case class RequestAllTemps(requestId: Long, groupId: String, replyTo: ActorRef[ReplyAllTemps])
-    extends DeviceGroupQuery.Command
-    with DeviceManager.Command
-    with DeviceGroup.Command
-
-  final case class ReplyAllTemps(requestId: Long, temps: Map[String, TempReading])
-
-  sealed trait TempReading
-  final case class Temp(value: Double) extends TempReading
-  case object TempNotAvailable extends TempReading
-  case object DeviceNotAvailable extends TempReading
-  case object DeviceTimeOut extends TempReading
 }
--- src/main/scala/com/iot/DeviceManager.scala
+++ src/main/scala/com/iot/DeviceManager.scala
@@ -19,6 +19,19 @@ object DeviceManager {
   //command for a terminated DeviceGroup
   final case class DeviceGroupTerminated(groupId: String) extends DeviceManager.Command
 
+  protected final case class RequestAllTemps(requestId: Long, groupId: String, replyTo: ActorRef[ReplyAllTemps])
+    extends DeviceGroupQuery.Command
+      with DeviceManager.Command
+      with DeviceGroup.Command
+
+  final case class ReplyAllTemps(requestId: Long, temps: Map[String, TempReading])
+
+  sealed trait TempReading
+  final case class Temp(value: Double) extends TempReading
+  case object TempNotAvailable extends TempReading
+  case object DeviceNotAvailable extends TempReading
+  case object DeviceTimeOut extends TempReading
+
   def apply(): Behavior[Command] =
     Behaviors.setup( new DeviceManager(_) )
 }
