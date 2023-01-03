f8f3d71552d2e1c94b6406cf4795fd3f4452be76
Tim <voncurleone@gmail.com>
2023-01-03 16:45:27 -0500

    Created DeviceGroupQuery Actor skeleton.

--- src/main/scala/com/iot/DeviceGroupQuery.scala
+++ src/main/scala/com/iot/DeviceGroupQuery.scala
@@ -1,7 +1,36 @@
 package com.iot
 
-import akka.actor.typed.ActorRef
+
+import akka.actor.typed.scaladsl.{AbstractBehavior, ActorContext, Behaviors, TimerScheduler}
+import akka.actor.typed.{ActorRef, Behavior}
+
+import scala.concurrent.duration.FiniteDuration
 
 object DeviceGroupQuery {
   trait Command
+
+  def apply(
+           devices: Map[String, ActorRef[Device.Command]],
+           requestId: Long,
+           requester: ActorRef[DeviceManager.ReplyAllTemps],
+           timeout: FiniteDuration ): Behavior[Command] = {
+    Behaviors.setup { context =>
+      Behaviors.withTimers { timers =>
+        new DeviceGroupQuery(devices, requestId, requester, timeout, context, timers)
+      }
+    }
+  }
+}
+
+class DeviceGroupQuery(
+                      devices: Map[String, ActorRef[Device.Command]],
+                      requestId: Long,
+                      requester: ActorRef[DeviceManager.ReplyAllTemps],
+                      timeout: FiniteDuration,
+                      context: ActorContext[DeviceGroupQuery.Command],
+                      timers: TimerScheduler[DeviceGroupQuery.Command]
+                      ) extends AbstractBehavior[DeviceGroupQuery.Command](context) {
+
+
+  override def onMessage(msg: DeviceGroupQuery.Command): Behavior[DeviceGroupQuery.Command] = ???
 }
