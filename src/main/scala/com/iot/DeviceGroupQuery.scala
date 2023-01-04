18b87e4c698309c58a5a1d4f6480e15bad8b20ce
Tim <voncurleone@gmail.com>
2023-01-03 23:16:59 -0500

    completed outline for DeviceGroupQuery actor.

--- src/main/scala/com/iot/DeviceGroupQuery.scala
+++ src/main/scala/com/iot/DeviceGroupQuery.scala
@@ -8,6 +8,9 @@ import scala.concurrent.duration.FiniteDuration
 
 object DeviceGroupQuery {
   trait Command
+  final case class WrappedRespondTemp(reply: Device.RespondTemp) extends Command
+  private case object ConnectionTimout extends Command
+  private final case class DeviceTerminated(deviceId: String) extends Command
 
   def apply(
            devices: Map[String, ActorRef[Device.Command]],
@@ -30,7 +33,16 @@ class DeviceGroupQuery(
                       context: ActorContext[DeviceGroupQuery.Command],
                       timers: TimerScheduler[DeviceGroupQuery.Command]
                       ) extends AbstractBehavior[DeviceGroupQuery.Command](context) {
+  import DeviceGroupQuery._
 
+  timers.startSingleTimer(ConnectionTimout, ConnectionTimout, timeout)
+  private val responseAdapter = context.messageAdapter(WrappedRespondTemp.apply)
+
+  devices.foreach {
+    case (deviceId, device) =>
+      context.watchWith(device, DeviceTerminated(deviceId))
+      device ! Device.ReadTemp(0, responseAdapter)
+  }
 
   override def onMessage(msg: DeviceGroupQuery.Command): Behavior[DeviceGroupQuery.Command] = ???
 }
